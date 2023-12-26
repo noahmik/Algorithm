63967e1b6514999e7abfb7c0385894ef53719266
noahmik <s46422574@gmail.com>
2023-12-26 14:45:01 +0900

    STACK 후위표기응용

--- 자료구조.py
+++ 자료구조.py
@@ -1,36 +1,81 @@
-class Stack:
-    def __init__(self, stack_size): # 스택을 생성합니다
-        self.stack = [None] * stack_size
-				# 생성시 받은 매개변수(stack_size)의 값만큼 스택 공간을 생성합니다. 
-        self.top = -1  # 스택의 가장 위를 나타내는 변수입니다. 초기값은 -1로 설정합니다.
+class Stack():
+    def __init__(self, Stack_Size):
+        self.top = -1  # 스택의 가장 위를 가리키는 인덱스
+        self.Stack_Size = Stack_Size  # 스택의 크기
+        self.list = [None]  # 스택을 저장할 리스트
 
-    def is_empty(self):
-        return self.top == -1  
-				# 스택이 비어있는지 확인하기 위해 초기값인 self.top이 초기값인 -1인지 확인합니다.
+    def isEmpty(self):
+        return self.top == -1  # 스택이 비어있는지 확인
 
-    def is_full(self):
-        return self.top == len(self.stack) - 1  
-				# 스택이 가득 찼는지 확인하기 위해 현재위치인 self.top과 스택 전체 사이즈인 
-				#	self.stack을 비교합니다.
+    def isFull(self):
+        return self.top == self.Stack_Size - 1  # 스택이 가득 찼는지 확인
 
-    def push(self, item):
-        if self.is_full():  # 스택이 가득 찼을 경우 오류 메시지를 출력합니다.
+    def push(self, data):
+        if(self.isFull()):  # 스택이 가득 찼을 경우
             print("스택이 가득 찼습니다")
-        else:
-            self.top += 1  # 스택의 가장 위를 한 칸 올립니다.
-            self.stack[self.top] = item  # 스택에 새로운 요소를 추가합니다.
+            return None
+        self.top += 1  # top 인덱스를 증가시키고
+        self.list[self.top] = data  # 해당 위치에 데이터를 저장합니다.
 
     def pop(self):
-        if self.is_empty():  # 스택이 비어있을 경우 오류 메시지를 출력합니다.
+        if(self.isEmpty()):  # 스택이 비어있을 경우
             print("스택이 비었습니다")
-        else:
-            item = self.stack[self.top]  # 스택의 가장 위에 있는 요소를 가져옵니다.
-            self.stack[self.top] = None  # 스택에서 해당 요소를 제거합니다.
-            self.top -= 1  # 스택의 가장 위를 한 칸 내립니다.
-            return item  # 제거한 요소를 반환합니다.
+            return None
+        data = self.list[self.top]  # top 위치의 데이터를 가져옵니다.
+        self.top -= 1  # top 인덱스를 감소시킵니다.
+        return data  # 가져온 데이터를 반환합니다.
 
     def peek(self):
-        if self.is_empty():  # 스택이 비어있을 경우 오류 메시지를 출력합니다.
+        if(self.isEmpty()):  # 스택이 비어있을 경우
             print("스택이 비었습니다")
+            return None
+        return self.list[self.top]  # top 위치의 데이터를 반환합니다.
+
+
+def precedence(op):
+    if op == '(' or op == ')':
+        return 0  # 괄호의 우선순위는 가장 낮습니다.
+    elif op == '+' or op == '-':
+        return 1  # 덧셈과 뺄셈의 우선순위는 낮습니다.
+    elif op == '*' or op == '/':
+        return 2  # 곱셈과 나눗셈의 우선순위는 높습니다.
+    else:
+        return -1  # 연산자가 아닌 경우
+
+
+# 중위표기 -> 후위표기로 바꾸는 함수
+def Infix2Postfix(expr):
+    s = Stack(int(input("Stack의 크기: ")))  # 스택 객체를 생성합니다.
+    output = []  # 후위표기식을 저장할 리스트
+
+    for data in expr:
+        if data in '(':
+            s.push('(')  # 여는 괄호를 스택에 push합니다.
+        elif data in ')':
+            while not s.isEmpty():
+                op = s.pop()
+                if op == '(':
+                    break  # 닫는 괄호를 만날 때까지 스택에서 pop하여 출력합니다.
+                else:
+                    output.append(op)
+        elif data in "+-*/":
+            while not s.isEmpty():
+                op = s.peek()
+                if precedence(op) >= precedence(data):
+                    output.append(op)
+                    s.pop()
+                else:
+                    break  # 스택의 연산자의 우선순위가 현재 연산자보다 높거나 같을 때까지 pop합니다.
+            s.push(data)  # 현재 연산자를 스택에 push합니다.
         else:
-            return self.stack[self.top]  # 스택의 가장 위에 있는 요소를 반환합니다.
+            output.append(data)  # 피연산자는 바로 출력 리스트에 추가합니다.
+
+    while not s.isEmpty():
+        output.append(s.pop())  # 스택에 남아있는 모든 연산자를 출력 리스트에 추가합니다.
+
+    return output
+
+
+a = list(input("수식을 입력하시오: "))  # 수식을 리스트로 입력받습니다.
+b = Infix2Postfix(a)  # 중위표기식을 후위표기식으로 변환합니다.
+print("입력한 수식의 후위표기식은 {}입니다.".format(*b))  # 후위표기식을 출력합니다.
