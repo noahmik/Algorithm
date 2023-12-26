152b00c93927f6a4ccee04fbec54bfcf69859150
noahmik <s46422574@gmail.com>
2023-12-26 14:46:38 +0900

    원형 QUEUE

--- 자료구조.py
+++ 자료구조.py
@@ -1,36 +1,34 @@
-class LinearQueue:
-    def __init__(self, capacity):
+class CircularQueue:
+    def __init__(self, capacity=5):
         self.capacity = capacity  # 큐의 최대 용량 설정
-        self.queue = [None] * capacity  # 큐를 배열로 구현
-        self.front = 0  # 큐의 시작 인덱스
-        self.rear = -1  # 큐의 끝 인덱스
-        self.size = 0  # 현재 큐에 저장된 데이터의 개수
+        self.list = [None] * capacity  # 큐를 표현하는 리스트
+        self.front = 0  # 큐의 시작 위치
+        self.rear = 0  # 큐의 마지막 요소 다음 위치
 
-    def is_empty(self):
-        return self.size == 0  # 큐가 비어있는지 확인
+    def isEmpty(self):
+        return self.front == self.rear  # 큐가 비어있는지 확인
 
-    def is_full(self):
-        return self.size == self.capacity  # 큐가 가득 찼는지 확인
+    def isFull(self):
+        return (self.rear + 1) % self.capacity == self.front  # 큐가 가득 찼는지 확인
 
-    def enqueue(self, data):
-        if self.is_full():
-            print("큐가 가득 찼습니다.") #큐가 가득찼다면 enqueue 불가능
-        else:
-            self.rear = (self.rear + 1) % self.capacity  # rear 값을 증가시키고, 배열의 끝에 도달하면 다시 처음으로 돌아감
-            self.queue[self.rear] = data  # rear 위치에 데이터 저장
-            self.size += 1  # 데이터 개수 증가
+    def enqueue(self, item):
+        if self.isFull():
+            print("큐가 가득 찼습니다.")
+            return False
+        self.list[self.rear] = item  # rear 위치에 요소 추가
+        self.rear = (self.rear + 1) % self.capacity  # rear 위치 업데이트
+        return True
 
     def dequeue(self):
-        if self.is_empty():
-            print("큐가 비어있습니다.") #큐가 비었다면 dequeue 불가능
-        else:
-            data = self.queue[self.front]  # front 위치에 있는 데이터 추출
-            self.front = (self.front + 1) % self.capacity  # front 값을 증가시키고, 배열의 끝에 도달하면 다시 처음으로 돌아감
-            self.size -= 1  # 데이터 개수 감소
-            return data
+        if self.isEmpty():
+            print("큐가 비어있습니다.")
+            return None
+        item = self.list[self.front]  # front 위치의 요소 추출
+        self.list[self.front] = None  # front 위치의 요소 삭제
+        self.front = (self.front + 1) % self.capacity  # front 위치 업데이트
+        return item
 
     def peek(self):
-        if self.is_empty():
-            print("큐가 비어있습니다.") #큐가 비었다면 peek 불가능
-        else:
-            return self.queue[self.front]  # front 위치에 있는 데이터 반환
+        if self.isEmpty():
+            return None
+        return self.list[self.front]  # front 위치의 요소 반환
