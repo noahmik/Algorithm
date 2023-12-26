8066db05c254db5c92ce963e739d33172ac5f217
noahmik <s46422574@gmail.com>
2023-12-26 14:47:32 +0900

    원형 DEQUE

--- 자료구조.py
+++ 자료구조.py
@@ -1,82 +1,77 @@
 class Deque: #Deque 클래스 지정
     #클래스를 찍어낼 때 초기화 시키는 함수
-    def __init__(self, Deque_size): 
+    def __init__(self, Deque_size):
         self.Deque_size = Deque_size #Deque의 크기
-        self.front = -1  #Deque의 앞쪽 위치를 가리키는 변수
-        self.rear = self.Deque_size #Deque의 뒤쪽 위치를 가리키는 변수
-        self.list = [None]*Deque_size  #Deque에 들어갈 값들이 저장될 곳을 Duque의 크기만큼 설정해준다
+        self.front = 0 #Deque의 앞쪽 위치를 가리키는 변수
+        self.rear = 0 #Deque의 뒤쪽 위치를 가리키는 변수
+        self.list = [None] * Deque_size #Deque에 들어갈 값들이 저장될 곳을 Duque의 크기만큼 설정해준다
     
     #Deque이 비어있는지 확인하는 함수
-    def isEmpty(self): 
-        #rear가 Deque의 크기와 같고 front가 -1이면 즉, 초기값이면 비어있다
+    def isEmpty(self):
+        #front와 rear가 같다면 비어있음
         #Deque가 비어있으면 True, 아니면 False를 반환
-        return self.rear == self.Deque_size and self.front == -1 
+        return self.front == self.rear
     
     #Deque이 가득 찼는지 확인하는 함수
-    def isFull(self): 
-        #front가 Deque의 크기-1 즉, 리스트의 마지막 인덱스이거나 rear가 0이면 가득참
+    def isFull(self):
+        #rear를 1증가시키고 Deque의 크기로 나눈 나머지가 front와 같으면 가득참
         #Deque이 가득찼으면 True, 아니면 False를 반환
-        return self.front == self.Deque_size-1 or self.rear == 0 
+        return self.front == (self.rear + 1) % self.Deque_size
     
     #Deque의 앞쪽에 값를 추가하는 함수
-    def AddFront(self, item): 
-        #Deque이 가득 찼다면 "FULL"을 출력
-        if self.isFull(): 
+    def Addfront(self, item):
+        #Deque가 가득 찼다면 "FULL"을 출력
+        if self.isFull():
             return print("FULL")
         #Deque이 가득 차지 않았다면
-        else: 
-            self.front += 1 #front를 1증가 시키고
-            self.list[self.front] = item  #front가 증가된 인덱스 위치에 값을 추가한다
+        else:
+            self.list[self.front] = item #front에 받은 값을 추가한다
+            #front를 -1하고 Deque 크기를 더한 후 Deque의 크기로 나눈 나머지를 front에 넣는다
+            self.front = (self.front - 1 + self.Deque_size) % self.Deque_size
         
+    #Deque의 뒤쪽에 값를 추가하는 함수
+    def Addrear(self, item):
+        #Deque이 가득 찼다면 "FULL"을 출력
+        if self.isFull():
+            return print("FULL")
+        #그렇지 않다면
+        else:
+            #rear를 +1한 후 Deque의 크기로 나눈 나머지를 rear에 넣는다
+            self.rear = (self.rear + 1) % self.Deque_size 
+            self.list[self.rear] = item #rear의 위치에 받은 값을 추가 시킨다
+            
     #Deque의 앞쪽 값을 삭제하는 함수
-    def DeleteFront(self): 
-        #Deque가 비었다면 "EMPTY"를 출력
+    def Deletefront(self):
+        #Deque이 비었다면 "EMPTY"를 출력
         if self.isEmpty():
             return print("EMPTY")
         #그렇지 않다면
         else:
-            data = self.list[self.front] #data에 front에 있는 값을 넣는다
-            self.list[self.front] = None #front에 값을 None으로 바꾼다
-            self.front -= 1 #front를 1감소시킨다
-            
-            return data #삭제한 값을 반환한다
+            #front를 +1한 후 Deque의 크기로 나눈 나머지를 front에 넣는다
+            self.front = (self.front + 1) % self.Deque_size
+            return self.list[self.front]
+    
+    def Deleterear(self):
+        # 덱의 뒤쪽에서 원소를 삭제함
+        if self.isEmpty():
+            return print("EMPTY")
+        else: 
+            #rear를 -1하고 Deque 크기를 더한 후 Deque의 크기로 나눈 나머지를 rear에 넣는다
+            self.rear = (self.rear - 1 + self.Deque_size) % self.Deque_size
+            return self.list[self.rear]
     
     #Deque의 앞쪽 값을 반환하는 함수
-    def GetFront(self): 
+    def Getfront(self):
         #front 위치의 값을 반환한다
-        return self.list[self.front] 
-    
-    #Deque의 뒤쪽에 값을 추가하는 함수
-    def AddRear(self, item): 
-        #Deque이 가득 찼다면
-        if self.isFull():
-            #"FULL"을 출럭
-            return print("FULL")
-        #그렇지 않다면
-        else:
-            #rear를 한 칸 뒤로 이동시키고
-            self.rear -= 1
-            #거기에 받은 값을 추가한다
-            self.list[self.rear] = item
-    
-    #Deque의 뒤쪽 값을 삭제하는 함수
-    def DeleteRear(self): 
-        #Deque이 비어있다면
-        if self.isEmpty():
-            return print("EMPTY") #"EMPTY"를 출력
-        #그렇지 않다면
-        else:
-            data = self.list[self.rear] #data에 rear에 있는 값을 넣는다 
-            self.list[self.rear] = None #rear에 값을 None으로 바꾼다
-            self.rear += 1 #rear를 1증가시킨다
-            return data #삭제한 값을 반환한다
+        return self.list[(self.front + 1) % self.Deque_size]
     
     #Deque의 뒤쪽 값을 반환하는 함수
-    def GetRear(self): 
-        #rear 위치의 값을 반환한다
-        return self.list[self.rear] 
-    
-de = Deque(int(input("덱의 사이즈를 입력하시오: ")))
+    def Getrear(self):
+            #rear 위치의 값을 반환한다
+            return self.list[self.rear]
+
+
+de = Deque(int(input("Deque의 사이즈: ")))
 
 # 앞쪽에 원소 추가
 for i in range(1, 4): #i가 1부터 4보다 작을 때까지 1씩 증가하면서 for문을 돈다
