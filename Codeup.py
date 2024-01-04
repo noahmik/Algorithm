f3f4303c7262734dbe9cc0ccc896b4b1c518ea4c
noahmik <s46422574@gmail.com>
2024-01-04 13:28:47 +0900

    codeup 1920

--- Codeup.py
+++ Codeup.py
@@ -1,9 +1,13 @@
-a = int(input())
+def trans(n):
+    if (n<1):
+        return '0'
+    elif (n==1):
+        return '1'
+    if (n%2==0):
+        return trans(int(n/2)) + '0'
+    elif (n%2==1):
+        return trans(int(n/2)) + '1'
 
-def cir(num):
-    if num == 1:
-        return 1
-        
-    return num*cir(num-1)
-
-print(cir(a))
+n = int(input())
+answer = trans(n)
+print(answer)
