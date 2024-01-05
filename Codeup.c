4ab851ab225bde207a031fbd6fd713d2cd7ccb30
noahmik <s46422574@gmail.com>
2024-01-05 13:25:45 +0900

    codeup 1420

--- Codeup.c
+++ Codeup.c
@@ -1,28 +1,28 @@
-#include<stdio.h>
- 
-int main(){
-    
-    int i_num;
-    scanf("%d",&i_num);
-    
-    int bin_arr[1000] = {};
-    int i_Cnt = 0;
-    
-    for(int i=0; i_num > 0; i++ ){
-        
-        bin_arr[i] = i_num % 2;
-        i_num /= 2;
-        i_Cnt++;
+#include <stdio.h>
+int main()
+{
+  char a[50][50];
+  int b[50],c[50],d;
+
+  scanf("%d",&d);
+  for(int i=1;i<=d;i++){
+    scanf("%s %d",a[i],&b[i]); //2차원 배열에서는 & 쓰지않음
+    c[i] = 1; // 시작은 1
+  
+  }
+  for(int j=1;j<=d;j++){
+    for(int x=1;x<=d;x++){
+      if(b[j]<b[x]){
+        c[j]++;
+      } 
     }
-    
-    i_Cnt -= 1;
-    if(i_Cnt < 0){printf("%d",bin_arr[0]);}
-    else{
-        for(int j = i_Cnt; j>=0; j--){
-            
-            printf("%d",bin_arr[j]);
-        }
+  }
+  for(int y=1;y<=d;y++){
+    if(c[y]==3){
+      printf("%s",a[y]);
     }
-    return 0;
+  }
+
+  
 }
- 
+
