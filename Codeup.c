d756bf73f1a172be67a50f42b6be50f466e8231f
noahmik <s46422574@gmail.com>
2024-01-07 13:22:59 +0900

    codeup 1411

--- Codeup.c
+++ Codeup.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
-int main() {
 
-    int a[11],b;
-    for(int i=1;i<=10;i++) {
-    scanf("%d",&a[i]);
+    int main(){       
+    int a[51]={},b,c,d;
+    scanf("%d",&b);
+    for(int i=1;i<=b;i++) {
+        scanf("%d",&d);
+        a[d]=1;
+    } 
+    for(int j=1;j<=b;j++) {
+        if(a[j]==0){
+            c=j;
+        }
     }
 
-    scanf("%d",&b);
-    printf("%d",a[b]);
-
+    printf("%d",c);
 
     return 0;
 }
-
