2b494f51335a404bd99af267a95b953dde3a5c77
noahmik <s46422574@gmail.com>
2024-01-08 13:58:39 +0900

    pjh 예외처리 예시

--- problem.java
+++ problem.java
@@ -1,71 +1,15 @@
-abstract class Shape {
-    Point p;
-    
-    Shape(){
-        this(new Point(0,0));
-    }
+class pjh {
+    public static void main(String args[]) {
+        int result = 0;
+        try {
+            int number = 100;
 
-    Shape(Point p) {
-        this.p = p;
-    }
-
-    abstract double clacArea();
-
-    Point getPosition() {
-        return p;
-    }
-
-    void setPosiion(Point p) {
-        this.p = p;
-    }
-}
-
-class Point {
-    int x;
-    int y;
-    
-    Point(){
-        this(0,0);
-    }
-    
-    Point(int x,int y){
-        this.x = x;
-        this.y = y;
-    }
-    
-    public String toString(){
-        return "["+x+","+y+"]";
-    }
-}
-
-class Circle extends Shape{
-    double r; // 반지름
-
-    Circle(double r){
-        super();
-        this.r = r;
-    }
-    @Override
-    double clacArea() {
-        return Math.PI * r * r;
-    }
-}
-
-class Rectangle extends Shape{
-    double width;
-    double height;
-    Rectangle(double width,double height){
-        this.width = width;
-        this.height = height;
-    }
-    @Override
-    double clacArea() {
-        return width*height;
-    }
-    boolean isSquare(){
-        if (width == height){
-            return true;
+            for (int i = 0; i < 10; i++) {
+                result = number / (int) (Math.random() * 10);
+                System.out.println(result);
+            }
+        } catch (ArithmeticException e) {
+            result = 0;           
         }
-        return false;
     }
 }
