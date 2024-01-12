121a89a364418ed7b4c4eab760aaa1f1a0be29eb
noahmik <s46422574@gmail.com>
2024-01-12 23:55:00 +0900

    #2.1 Before React

--- Codeup.py
+++ Codeup.py
@@ -1,29 +1,5 @@
-change = int(input())
-count = 0
-while change:
-  if change >= 50000:
-    change -= 50000
-    count +=1
-  elif change >= 10000:
-    change -= 10000
-    count +=1
-  elif change >= 5000:
-    change -= 5000
-    count +=1
-  elif change >= 1000:
-    change -= 1000
-    count +=1
-  elif change >= 500:
-    change -= 500
-    count +=1
-  elif change >= 100:
-    change -= 100
-    count +=1
-  elif change >= 50:
-    change -= 50
-    count +=1
-  elif change >= 10:
-    change -= 10
-    count +=1
+pasta = [int(input()) for _ in range(3)]
+juice = [int(input()) for _ in range(2)]
+mini = min(pasta) + min(juice)
 
-print(count)
+print(f'{mini*1.1:.1f}')
