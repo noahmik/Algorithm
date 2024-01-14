69d3de77501e0fb4357881cbddccd301818e163a
noahmik <s46422574@gmail.com>
2024-01-14 22:48:18 +0900

    codeup 1610

--- Codeup.py
+++ Codeup.py
@@ -1,9 +1,6 @@
-def hanoi_tower(n, a, b, c):
-    if n > 0:
-        hanoi_tower(n-1, a, c, b)
-        print(f"Disk {n} : {a} to {b}")
-        hanoi_tower(n-1, c, b, a)
-
-n = int(input())
-hanoi_tower(n, 'A', 'C', 'B')
-
+def mysubstr(word,start,count):
+    for i in range(count):
+        print(word[start+i],end='')
+w=input()
+s,c=map(int,input().split())
+mysubstr(w,s,c)
