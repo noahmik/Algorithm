d1defe501ee8696be86f0ed6e32d871cda774710
noahmik <s46422574@gmail.com>
2024-01-17 09:58:01 +0900

    baekjoon 2292

--- BeakJoon.c
+++ BeakJoon.c
@@ -1,30 +1,21 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char s[100];
-    int alphabet[26], len, index;
-    
-    for (int i = 0; i < 26; i++) {
-        alphabet[i] = -1;
-    }
-    
-    scanf("%s", s);
-    
-    len = strlen(s);
-    for (int i = 0; i < len; i++) {
-        index = s[i] - 'a';
-        if (alphabet[index] == -1) {
-            alphabet[index] = i;
-        }
-        else {
-            continue;
-        }
-    }
-    
-    for (int i = 0; i < 26; i++) {
-        printf("%d ", alphabet[i]);
-    }
-    
-    return 0;
+	int n;
+	scanf("%d", &n);
+
+	int ret = 1;
+	int max = 1;
+	int add = 6;
+
+	while (1) {
+		if (n <= max) break;
+
+		max += add;
+		add += 6;
+		++ret;
+	}
+
+	printf("%d\n", ret);
+	return 0;
 }
