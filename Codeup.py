f5a86e0f9412040028d93aab07ee0fe5f9661c4e
noahmik <s46422574@gmail.com>
2024-01-21 09:32:15 +0900

    baekjoon 2908

--- Codeup.py
+++ Codeup.py
@@ -1,8 +1,24 @@
-n = int(input())
-students = list(map(int, input().split()))
+#include <stdio.h>
 
-rank = sorted(students, reverse=True)
+int f(int x){
+	int ans = 0;
+	
+	for(int i=x;i;i/=10)
+		ans = ans*10 + i%10;
+	
+	return ans;
+}
 
-for i in range(n):
-    a = rank.index(students[i])
-    print(students[i], a+1) 
+int main(){
+	int a,b;
+	
+	scanf("%d %d",&a,&b);
+	
+	a = f(a);
+	b = f(b);
+	
+	if(a>b) printf("%d\n",a);
+	else printf("%d\n",b);
+	
+	return 0;
+}
