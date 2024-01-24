22c4bfb5bd6b0b37286d38b5aada0137c7d4ce42
noahmik <s46422574@gmail.com>
2024-01-25 01:53:06 +0900

    codeup 4016

--- Codeup.py
+++ Codeup.py
@@ -1,9 +1,9 @@
-def 유클리드(a, b):
-    if b == 0:
-        return a
-    else:
-        return 유클리드(b, a % b)
+def 최대공약수(a,b,c):
+  max = 0
+  for i in range(1,a + 1):
+     if a % i == 0 and b % i == 0 and c % i ==0:
+        max = i
+  return max
 
-a, b = map(int, input().split())
-print(유클리드(a, b))
-
+a,b,c = map(int,input().split())
+print(최대공약수(a,b,c))
