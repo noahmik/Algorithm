8370fab2a10ff3bd421a6393b8435b148282a509
noahmik <s46422574@gmail.com>
2024-01-28 01:50:14 +0900

    codeup 1380

--- Codeup.py
+++ Codeup.py
@@ -1,28 +1,6 @@
-inputNumber = int(input())
-temp = inputNumber
-digit = 0
-rem = 0
-sum = 0
+A =int(input())
 
-while temp > 0:
-    digit += 1
-    temp //= 10
-
-digit2 = digit
-
-for i in range(digit):
-    if inputNumber % 10 == 0:
-        digit2 -= 1
-        inputNumber //= 10
-    else:
-        result = inputNumber
-        for i in range(digit2):
-            rem = result % 10
-            result //= 10
-            sum += rem
-            print(rem, end="")
-        print()
-        print(sum)
-        break
-
-
+for i in range(1,7):
+    for j in range(1,7):
+        if i + j == A:
+            print(i,j)
