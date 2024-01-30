9ae02f8b093cff67ccfd6d12dbac42871363787d
noahmik <s46422574@gmail.com>
2024-01-31 01:48:05 +0900

    codeup 1617

--- Codeup.py
+++ Codeup.py
@@ -1,4 +1,18 @@
-string = input()
+def is_palindrome(num):
+    # 입력받은 숫자를 뒤집은 후 더하기
+    reverse_num = int(str(num)[::-1])
+    sum_num = num + reverse_num
+    
+    # 회문인지 아닌지 판별
+    if str(sum_num) == str(sum_num)[::-1]:
+        return True
+    else:
+        return False
 
-for i in range(len(string)-1, -1, -1):
-    print(string[i], end='')
+n = int(input())
+
+if is_palindrome(n):
+    print("YES")
+else:
+    print("NO")
+
