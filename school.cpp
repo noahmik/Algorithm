95c7c05337017838c9a94394298649b6e27f80ff
noahmik <s46422574@gmail.com>
2024-02-20 14:48:07 +0900

    school EX09

--- school.cpp
+++ school.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
-#include <stack>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-bool Check(const string& s) {
-  stack<char> a;
+vector<int> count(int n) {
+  vector<int> counts(n + 1, 0); // 각 숫자의 약수 개수를 저장할 벡터 초기화
 
-  for (char c : s) {
-    if (c == '(') {
-      a.push(c);
-    } else if (c == ')') {
-      if (a.empty() || a.top() != '(') {
-        return false;
-      } else {
-        a.pop();
-      }
+  for (int i = 1; i <= n; ++i) {
+    for (int j = i; j <= n; j += i) {
+      counts[j]++; // j의 약수의 개수 증가
     }
   }
 
-  return a.empty();
+  return counts;
 }
 
 int main() {
-  string a;
-  cin >> a;
+  int N;
+  cin >> N;
 
-  if (Check(a)) {
-    cout << "YES" << endl;
-  } else {
-    cout << "NO" << endl;
+  vector<int> counts = count(N);
+
+  for (int i = 1; i <= N; ++i) {
+    cout << counts[i] << " ";
   }
+  cout << endl;
 
   return 0;
 }
