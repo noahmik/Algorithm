e98170b41d494f974155d9808f620d6693a638bd
noahmik <s46422574@gmail.com>
2024-02-22 14:49:22 +0900

    nypc 2018_1

--- school.cpp
+++ school.cpp
@@ -1,38 +1,27 @@
+// https://nypc.github.io/2018/2018_online_3
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
-int digit_sum(int x) {
-  int sum = 0;
-  while (x > 0) {
-    sum += x % 10;
-    x /= 10;
-  }
-  return sum;
-}
-
 int main() {
-  int N;
-  cin >> N;
-
-  vector<int> numbers(N);
-  for (int i = 0; i < N; ++i) {
-    cin >> numbers[i];
-  }
-
-  int max_sum = 0;
-  int max_number = 0;
-
-  for (int i = 0; i < N; ++i) {
-    int sum = digit_sum(numbers[i]);
-    if (sum > max_sum || (sum == max_sum && numbers[i] > max_number)) {
-      max_sum = sum;
-      max_number = numbers[i];
+  int P,Q,W;
+  int x,y;
+  cin >> P >> Q;
+  cin >> W;
+  for(x=0; x<=W ;x++){
+    for(y=0 ;y<=W ;y++){
+      if((P*x) + (Q*y) == W){
+        cout << x <<" "<< y <<endl;
+        return 0;
+      }
     }
   }
-
-  cout << max_number << endl;
-
-  return 0;
 }
+
+//선생님 코드
+//for(x = 1;p*x<=w;x++){
+//if((w-(p*x))%q ==0){
+//cout << x << " " << (w-(p*x)) /q <<endl;
+//break;
+//}
+//}
