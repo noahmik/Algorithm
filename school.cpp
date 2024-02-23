be5864bcddad3e6cd3ddb3d6e506955d767ef8b8
noahmik <s46422574@gmail.com>
2024-02-23 14:51:08 +0900

    nypc 2018_2 Player 클래스 구현

--- school.cpp
+++ school.cpp
@@ -1,27 +1,46 @@
-// https://nypc.github.io/2018/2018_online_3
+// https://nypc.github.io/2018/2018_online_4
 #include <iostream>
-
+#include <utility>
+#include <vector>
 using namespace std;
 
-int main() {
-  int P,Q,W;
-  int x,y;
-  cin >> P >> Q;
-  cin >> W;
-  for(x=0; x<=W ;x++){
-    for(y=0 ;y<=W ;y++){
-      if((P*x) + (Q*y) == W){
-        cout << x <<" "<< y <<endl;
-        return 0;
-      }
-    }
-  }
-}
 
-//선생님 코드
-//for(x = 1;p*x<=w;x++){
-//if((w-(p*x))%q ==0){
-//cout << x << " " << (w-(p*x)) /q <<endl;
-//break;
-//}
-//}
+//선수 클래스
+class Player{
+private:
+    //팀 종류(Blue,Red)
+    string teamType;
+
+    //점수(0-10)
+    int score;
+
+    //기록(double(nn::nn)) - 초 단위로 바꿈
+    double record;
+public:
+    //생성자
+    explicit Player(string teamType ="", int score=0, double record=0.0) : teamType(std::move(teamType)), score(score), record(record) {}
+    //getter,setter
+    const string &getTeamType() const {
+      return teamType;
+    }
+
+    void setTeamType(const string &teamType) {
+      Player::teamType = teamType;
+    }
+
+    int getScore() const {
+      return score;
+    }
+
+    void setScore(int score) {
+      Player::score = score;
+    }
+
+    double getRecord() const {
+      return record;
+    }
+
+    void setRecord(double record) {
+      Player::record = record;
+    }
+};
