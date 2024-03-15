19366fe5b2828db21fae82e102edf0c69b93bbe4
noahmik <s46422574@gmail.com>
2024-03-15 23:09:45 +0900

    cpp 벡터 예제

--- problem.cpp
+++ problem.cpp
@@ -1,39 +1,84 @@
-// explicit : 명시적인, implicit : 묵시적
-// ex) explicit conversion(명시적 형변환) - 컴파일러가 헷갈리게 하지 않기 위해서 개발자가 직접 형변환 시키는 경우
-// int a = 3;
-// int b = 4;
-// double c;
-// c = (double)a/b;
-// ex) implicit conversion(묵시적 형변환) - 컴파일러가 자료형이 안 맞는 상황에서 지가 알아서 형변환 시키는 경우
-// int a = 3;
-// int b = 4;
-// double c;
-// c = a/b;
-
-//결론 : 명시적으로 프로그래밍을 하는 것이 좋은 코드다.
-
-
 #include <iostream>
+#include <cstdlib>
+#include <vector>
 using namespace std;
 
-class Cat{
-public:
-    //생성자의 매개변수가 하나일 때는 '=' 연산자가 가능하기 때문에 묵시적 표현을 막기위해
-    //생성자 앞에 explicit을 적어줘야한다.
-    //생성자의 매개변수가 두개 이상일 때는 explicit을 붙여줄 필요가 없다.
-    explicit Cat(string name,int age) : mName{std::move(name)}, mAge{age}{} ;
-    void printCatInfo(){
-      cout<<"고양이 이름은: "<<mName<<endl;
-    };
-private:
-    string mName;
-    int mAge;
-};
-
 int main() {
-  string str = "kitty";
-  int num = 3;
-  Cat kitty{str,num};
-  kitty.printCatInfo();
+  //STL 벡터를 사용하지 않는 배열선언
+  int num[5];
+  for(int i = 0; i<5; i++){
+    num[i] = i;
+    cout << num[i] << " ";
+  }
+  cout<<endl;
+  //STL 벡터를 사용하지 않는 합공간 배열선언
+  //C언어 스타일 - malloc, free
+//  int* numPtr = (int*)malloc(sizeof (int)*5);
+//  free(numPtr)
+  //C++스타일 - new, delete
+  int *numPtr = new int[5];
+  for(int i = 0; i<5; i++){
+    numPtr[i] = i;
+    cout<<numPtr[i]<<" ";
+  }
+  cout<<endl;
+  delete []numPtr;
+
+  //STL 벡터를 사용할 경우(힙공간)
+  vector<int> nums(5);
+  for(int i = 0; i<5; i++){
+    nums[i] = i;
+    cout<<nums[i]<<" ";
+  }
+  cout<<endl;
+
+  //벡터 초기화 2
+  vector<int> num2{1,2,3,4,5};
+
+
+  //벡터 내장 함수들
+  cout<<"----요소 삽입 전----"<<endl;
+  cout<<"벡터 요소의 개수: "<<nums.size()<<endl;
+  nums.emplace_back(5); //벡터의 마지막공간에 삽입
+  cout<<"----요소 삽입 후----"<<endl;
+  for(size_t i = 0; i<nums.size(); i++){
+    cout<<nums[i]<<" ";
+  }
+
+  cout<<endl;
+  nums.pop_back(); // 벡터의 마지막 요소 제거
+  cout<<"---요소 제거 후---"<<endl;
+  for(size_t i = 0; i<nums.size(); i++){
+    cout<<nums[i]<<" ";
+  }
+
+  //벡터 순회방법1 - 인덱스로 접근
+  for(size_t idx=0; idx<nums.size(); idx++){
+    cout<<nums[idx]<<" ";
+  }
+  cout<<endl;
+
+  //벡터 이터레이터
+  vector<int>::iterator itr; //벡터를 순회할 때 사용하는 포인터
+  // nums.begin(); : 첫번째 요소의 주솟값
+  // nums.end():  마지막 요소의 주솟값=1
+  itr = nums.begin();
+  cout<<"첫번째 요소의 값은? : "<<*itr<<endl;
+  itr = nums.end();
+  cout<<"마지막 요소의 값은? : "<<*(--itr)<<endl;
+
+  //벡터 순회방법2 - 이터레이터 활용(많이 활용)
+//  vector<int>::iterator itr2;
+  for(auto itr2= nums.begin();itr2<nums.end();itr2++){
+    cout<<*itr2<<" ";
+  }
+  cout<<endl;
+
+  //벡터 순회 방법 3 - range for(가장 깔끔)
+  for (auto num : nums){
+    cout<<num<<" ";
+  }
+  cout<<endl;
+
   return 0;
 }
