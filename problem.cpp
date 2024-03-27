51934ec0829f7994372103a532158e34103f6ec0
noahmik <s46422574@gmail.com>
2024-03-27 13:23:03 +0900

    school EX11

--- problem.cpp
+++ problem.cpp
@@ -1,33 +1,23 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
+int sum = 0;
 
-vector<int> count(int n) {
-  vector<int> cs(n + 1, 0); // 각 숫자의 약수 개수를 저장할 벡터 초기화
-
-  for (int i = 1; i <= n; ++i) {
-    for (int j = i; j <= n; j += i) {
-      cs[j]++; // j의 약수의 개수 증가
-    }
+void digit(int num){
+  sum+=1;
+  if(num/10>0){
+    num = num/10;
+    digit(num);
   }
-
-  return cs;
 }
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
+int main(){
   int N;
   cin >> N;
 
-  vector<int> counts = count(N);
-
-  for (int i = 1; i <= N; ++i) {
-    cout << counts[i] << " ";
+  for(int i=1;i<=N;i++){
+    digit(i);
   }
-  cout << endl;
 
-  return 0;
+  cout << sum << endl;
 }
-//두 번째 반복문을 j += i로 바꾸면 i의 배수만 계산하므로 불필요한 반복이 줄어들어 시간 복잡도가 개선됨
