ba0ef41f99ca0e01cf91555913da4fe73fb71849
noahmik <s46422574@gmail.com>
2024-03-28 13:32:29 +0900

    school EX12

--- problem.cpp
+++ problem.cpp
@@ -1,23 +1,32 @@
 #include <iostream>
 
 using namespace std;
-int sum = 0;
 
-void digit(int num){
-  sum+=1;
-  if(num/10>0){
-    num = num/10;
-    digit(num);
+int countDigits(int num) {
+  int count = 0;
+  int base = 1;
+  int result = 0;
+
+  while (num / base > 0) {
+    int digit = (num / base) % 10; // 현재 자리의 숫자
+    result += digit * count; // 현재 자리의 숫자가 등장하는 횟수를 더함
+    if (digit > 1) {
+      result += base;
+    } else if (digit == 1) {
+      result += num % base + 1; // 현재 자리의 숫자가 1인 경우, 나머지 자릿수를 더함
+    }
+    base *= 10;
+    count++;
   }
+
+  return result;
 }
 
-int main(){
+int main() {
   int N;
   cin >> N;
 
-  for(int i=1;i<=N;i++){
-    digit(i);
-  }
+  cout << countDigits(N) << endl;
 
-  cout << sum << endl;
+  return 0;
 }
