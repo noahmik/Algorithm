f818257af66215b5d3c447cc11159069aa765b53
noahmik <s46422574@gmail.com>
2024-03-30 13:34:22 +0900

    school EX14

--- problem.cpp
+++ problem.cpp
@@ -1,26 +1,48 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+
 using namespace std;
+//자연수 n이 소수인지 판별할 때 for문은 n의 ‘제곱근’까지만 반복한다.
+
+int reverse(int x) {
+  int reversed = 0;
+  while (x > 0) {
+    int digit = x % 10;
+    reversed = reversed * 10 + digit;
+    x /= 10;
+  }
+  return reversed;
+}
+
+bool isPrime(int x) {
+  if (x <= 1) return false;
+  if (x == 2) return true;
+  if (x % 2 == 0) return false;
+
+  for (int i = 3; i <= sqrt(x); i += 2) {
+    if (x % i == 0) return false;
+  }
+
+  return true;
+}
 
 int main() {
-  vector<int> N(10);
-  long long int a;
-  int max_num = 0;
-  int max_size = 0;
-  cin >> a;
-  while(a){
-    N[a%10] +=1;
-    a/=10;
+  int N;
+  cin >> N;
+
+  vector<int> numbers(N);
+  for (int i = 0; i < N; ++i) {
+    cin >> numbers[i];
   }
 
-  for(int i=0;i<=10;i++){
-    if(max_size<N[i]){
-      max_size = N[i];
-      max_num = i;
-    } else if(max_size == N[i]){
-      if(max_num<i){
-        max_num = i;
-      }
+  for (int i = 0; i < N; ++i) {
+    int reversed = reverse(numbers[i]);
+    if (isPrime(reversed)) {
+      cout << reversed << " ";
     }
   }
-  cout << max_num << endl;
+  cout << endl;
+
+  return 0;
 }
