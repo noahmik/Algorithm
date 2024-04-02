413e46b7090b5b323379f1f690d7d56bc00151ba
noahmik <s46422574@gmail.com>
2024-04-02 13:35:04 +0900

    school EX17

--- problem.cpp
+++ problem.cpp
@@ -1,32 +1,48 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 
 using namespace std;
 
-bool areAnagrams(const string& word1, const string& word2) {
-  if (word1.length() != word2.length()) {
-    return false;
+// 주어진 num이 정답 answer와 일치하는지 확인하는 함수
+bool is_correct(int num, int answer) {
+  // 1부터 num까지의 합을 구함
+  int sum = num * (num + 1) / 2;
+
+  // 정답과 계산된 합이 일치하는지 확인 후 반환
+  if (sum == answer) {
+    return true;
   }
 
-  // 두 단어를 정렬하여 순서를 맞춘 후 비교
-  string sorted_word1 = word1;
-  string sorted_word2 = word2;
-  sort(sorted_word1.begin(), sorted_word1.end());
-  sort(sorted_word2.begin(), sorted_word2.end());
-
-  return sorted_word1 == sorted_word2;
+  // 정답이 아니라면 false 반환
+  return false;
 }
 
-int main() {
-  string str1, str2;
-  cin >> str1 >> str2;
+// 문제를 해결하는 함수
+void solve(int N) {
+  // 종료 조건: N이 0이 되면 함수 종료
+  if (N == 0) {
+    return;
+  }
 
-  if (areAnagrams(str1, str2)) {
+  int card, answer;
+  cin >> card >> answer;
+
+  // 정답 여부 확인 후 출력
+  if (is_correct(card, answer)) {
     cout << "YES" << endl;
   } else {
     cout << "NO" << endl;
   }
+
+  // 다음 입력을 처리하기 위해 재귀 호출
+  solve(N - 1);
+}
+
+int main() {
+  int N;
+  cin >> N;
+
+  // 문제 해결 함수 호출
+  solve(N);
 
   return 0;
 }
