f2e742ac9b89b1b689d11d10ad08d3ea3e20b471
noahmik <s46422574@gmail.com>
2024-04-06 13:40:40 +0900

    school EX20

--- problem.cpp
+++ problem.cpp
@@ -7,23 +7,26 @@ int main() {
   int N;
   cin >> N;
 
-  vector<int> heights(N);
+  vector<int> A(N);
+  vector<int> B(N);
+
   for (int i = 0; i < N; ++i) {
-    cin >> heights[i];
+    cin >> A[i];
   }
 
-  int anger_inducers = 0; // 분노 유발자 수
-  int max_height = heights[N - 1]; // 가장 뒤에 있는 학생의 키
+  for (int i = 0; i < N; ++i) {
+    cin >> B[i];
+  }
 
-  // 뒤에서부터 순회하며 분노 유발자인지 확인
-  for (int i = N - 2; i >= 0; --i) {
-    if (heights[i] > max_height) {
-      anger_inducers++; // 분노 유발자인 경우 수를 증가
-      max_height = heights[i]; // 가장 큰 키를 갱신
+  for (int i = 0; i < N; ++i) {
+    if (A[i] == B[i]) {
+      cout << "D" << endl;
+    } else if ((A[i] == 1 && B[i] == 3) || (A[i] == 2 && B[i] == 1) || (A[i] == 3 && B[i] == 2)) {
+      cout << "A" << endl;
+    } else {
+      cout << "B" << endl;
     }
   }
-
-  cout << anger_inducers << endl;
 
   return 0;
 }
