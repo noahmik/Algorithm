61c7611ce836f3dfcdebdba32f719d20e229cb05
noahmik <s46422574@gmail.com>
2024-04-09 12:21:15 +0900

    정보 올림피아드 문제 :: 카드게임

--- problem.cpp
+++ problem.cpp
@@ -4,29 +4,39 @@
 using namespace std;
 
 int main() {
-  int N;
-  cin >> N;
-
-  vector<int> A(N);
-  vector<int> B(N);
-
-  for (int i = 0; i < N; ++i) {
+  vector<int> A(10);
+  vector<int> B(10);
+  int A_SCORE = 0;
+  int B_SCORE = 0;
+  for(int i=0;i<=9;i++){
     cin >> A[i];
   }
-
-  for (int i = 0; i < N; ++i) {
-    cin >> B[i];
+  for(int j=0;j<=9;j++){
+    cin >> B[j];
   }
 
-  for (int i = 0; i < N; ++i) {
-    if (A[i] == B[i]) {
-      cout << "D" << endl;
-    } else if ((A[i] == 1 && B[i] == 3) || (A[i] == 2 && B[i] == 1) || (A[i] == 3 && B[i] == 2)) {
-      cout << "A" << endl;
-    } else {
-      cout << "B" << endl;
+  for(int l=0;l<=9;l++){
+    if(A[l]>B[l]){
+      A_SCORE = A_SCORE + 3;
+    } else if(A[l]<B[l]){
+      B_SCORE = B_SCORE + 3;
+    } else if(A[l]==B[l]){
+      A_SCORE = A_SCORE + 1;
+      B_SCORE = B_SCORE + 1;
     }
   }
 
-  return 0;
+  if (A_SCORE > B_SCORE) {
+    cout << A_SCORE << " ";
+    cout << B_SCORE << endl;
+    cout << "A";
+  } else if (A_SCORE < B_SCORE) {
+    cout << A_SCORE;
+    cout << B_SCORE << endl;
+    cout << "B";
+  } else if (A_SCORE == B_SCORE){
+    cout << A_SCORE;
+    cout << B_SCORE << endl;
+    cout << "D";
+  }
 }
