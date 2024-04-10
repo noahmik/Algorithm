a43e6ea9efa779ca006f10e1a3cbbbea21676dd7
noahmik <s46422574@gmail.com>
2024-04-10 12:22:46 +0900

    정보 올림피아드 문제 :: 온도의 최대값

--- problem.cpp
+++ problem.cpp
@@ -4,39 +4,21 @@
 using namespace std;
 
 int main() {
-  vector<int> A(10);
-  vector<int> B(10);
-  int A_SCORE = 0;
-  int B_SCORE = 0;
-  for(int i=0;i<=9;i++){
-    cin >> A[i];
-  }
-  for(int j=0;j<=9;j++){
-    cin >> B[j];
+  int A,B,MAX = -100,MAX_mid=0;
+  cin >> A >> B;
+  int *C = new int[A];
+  for(int i=0;i<A;i++){
+    cin >> C[i];
   }
 
-  for(int l=0;l<=9;l++){
-    if(A[l]>B[l]){
-      A_SCORE = A_SCORE + 3;
-    } else if(A[l]<B[l]){
-      B_SCORE = B_SCORE + 3;
-    } else if(A[l]==B[l]){
-      A_SCORE = A_SCORE + 1;
-      B_SCORE = B_SCORE + 1;
+  for(int j=0;j<A-B;j++){
+    for(int g=j;g<j+B;g++){
+      MAX_mid = MAX_mid + C[g];
     }
+    if(MAX_mid>MAX){
+      MAX = MAX_mid;
+    }
+    MAX_mid = 0;
   }
-
-  if (A_SCORE > B_SCORE) {
-    cout << A_SCORE << " ";
-    cout << B_SCORE << endl;
-    cout << "A";
-  } else if (A_SCORE < B_SCORE) {
-    cout << A_SCORE;
-    cout << B_SCORE << endl;
-    cout << "B";
-  } else if (A_SCORE == B_SCORE){
-    cout << A_SCORE;
-    cout << B_SCORE << endl;
-    cout << "D";
-  }
+  cout << MAX;
 }
