6a80eec7c25ff9fef9f5291fde764ede0d1e8863
noahmik <s46422574@gmail.com>
2024-04-12 12:26:25 +0900

    정보 올림피아드 문제 :: Jolly Jumpers

--- problem.cpp
+++ problem.cpp
@@ -4,22 +4,32 @@
 using namespace std;
 
 int main() {
-  int A;
-  int LTS=0,LTS_N=0;
+  int A,result=0;
+  bool N= false;
   cin >> A;
   int *B = new int[A];
+
   for(int i=0;i<A;i++){
     cin >> B[i];
   }
+
   for(int j=0;j<A;j++){
-    if(B[j]<=B[j+1]){
-      LTS_N = LTS_N + 1;
-      if(LTS<LTS_N){
-        LTS = LTS_N;
+    for(int g=0;g<A;g++){
+      if(!N){
+      if(B[j] - B[j+1] == B[g]){
+        result +=1;
+        N = true;
+      } else if (B[j+1] - B[j] == B[g]){
+        result +=1;
+        N = true;
       }
-    } else {
-      LTS_N = 0;
+     }
     }
+    N= false;
   }
-  cout << LTS+1;
+  if(result == A){
+    cout << "YES";
+  } else {
+    cout << "NO";
+  }
 }
