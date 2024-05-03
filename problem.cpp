ce54aa10c97d25f83778d938ae3d02a8288df2f1
noahmik <s46422574@gmail.com>
2024-05-03 12:24:13 +0900

    baekjoon 11659

--- problem.cpp
+++ problem.cpp
@@ -1,71 +1,29 @@
 #include <iostream>
-#include <string>
 #include <vector>
 
 using namespace std;
 
-int count_valid_passwords(const string& S, int P, const vector<int>& min_counts) {
-  int count = 0;
-  vector<int> counts(4, 0); // A, C, G, T의 개수를 저장하는 배열
-  int start = 0; // 윈도우 시작 인덱스
-  int end = 0; // 윈도우 끝 인덱스
-
-  // 초기 윈도우 설정
-  for (end = 0; end < P; ++end) {
-    char c = S[end];
-    if (c == 'A') counts[0]++;
-    else if (c == 'C') counts[1]++;
-    else if (c == 'G') counts[2]++;
-    else if (c == 'T') counts[3]++;
-  }
-
-  while (end <= S.length()) {
-    // 현재 윈도우가 조건을 만족하는지 확인
-    bool valid = true;
-    for (int i = 0; i < 4; ++i) {
-      if (counts[i] < min_counts[i]) {
-        valid = false;
-        break;
-      }
-    }
-    if (valid) count++; // 조건을 만족하는 경우 카운트 증가
-
-    // 다음 윈도우로 이동
-    char start_char = S[start];
-    if (start_char == 'A') counts[0]--;
-    else if (start_char == 'C') counts[1]--;
-    else if (start_char == 'G') counts[2]--;
-    else if (start_char == 'T') counts[3]--;
-
-    if (end < S.length()) {
-      char end_char = S[end];
-      if (end_char == 'A') counts[0]++;
-      else if (end_char == 'C') counts[1]++;
-      else if (end_char == 'G') counts[2]++;
-      else if (end_char == 'T') counts[3]++;
-    }
-
-    start++;
-    end++;
-  }
-
-  return count;
-}
-
 int main() {
-  int N, P;
-  cin >> N >> P; // DNA 문자열의 길이와 부분문자열의 길이 입력 받기
+  ios::sync_with_stdio(false); // C++의 입출력 스트림과 C의 입출력 버퍼를 동기화하지 않음
+  cin.tie(NULL); // cin과 cout이 번갈아가며 사용될 때의 성능을 향상시킴
+  cout.tie(NULL); // cin과 cout이 번갈아가며 사용될 때의 성능을 향상시킴
 
-  string S;
-  cin >> S; // DNA 문자열 입력 받기
+  int N, M, A, B;
+  cin >> N >> M;
+  vector<int> sum(N + 1, 0);
 
-  vector<int> min_counts(4); // A, C, G, T의 최소 등장 횟수를 저장하는 배열
-  for (int i = 0; i < 4; ++i) {
-    cin >> min_counts[i]; // 최소 등장 횟수 입력 받기
+  // 합 배열
+  for (int i = 1; i <= N; i++) {
+    int num;
+    cin >> num;
+    sum[i] = sum[i - 1] + num;
   }
 
-  int result = count_valid_passwords(S, P, min_counts); // 가능한 비밀번호의 종류 수 계산
-  cout << result << endl; // 결과 출력
+  for (int j = 1; j <= M; j++) {
+    cin >> A >> B;
+    int result = sum[B] - sum[A - 1];
+    cout << result << '\n';
+  } //endl 함수는 개행만 해주는 것이 아닌 내부 버퍼를 비워주는 역할도 함께 => 매우느림
 
   return 0;
 }
