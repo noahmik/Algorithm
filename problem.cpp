5d43d32c1166e3397585a12507a22333d0a44358
noahmik <s46422574@gmail.com>
2024-05-12 12:26:59 +0900

    baekjoon 11286

--- problem.cpp
+++ problem.cpp
@@ -1,23 +1,31 @@
 #include <iostream>
-#include <vector>
-#include <queue> queue<int> q;
-
-using namespace std;
+#include <queue>
+#include <cmath>
 
 int main() {
   int N;
-  queue<int> que; //queue 생성
-  cin >> N;
+  std::cin >> N;
 
-  for(int i=1;i<=N;i++){
-    que.push(i); //queue에 값들 순서대로 push
+  // 우선순위 큐를 선언
+  std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>, std::greater<std::pair<int, int>>> pq;
+
+  for (int i = 0; i < N; ++i) {
+    int x;
+    std::cin >> x;
+
+    // 정수 x를 입력받고, 0이 아니라면 우선순위 큐에 삽입
+    if (x != 0) {
+      pq.push({abs(x), x});
+    } else {
+      // 정수가 0이라면 우선순위 큐에서 가장 작은 값을 출력
+      if (pq.empty()) {
+        std::cout << "0\n";
+      } else {
+        std::cout << pq.top().second << "\n";
+        pq.pop();
+      }
+    }
   }
 
-  while (que.size() > 1) { //queue에 요소가 하나만 남을때까지 pop 반복
-    que.pop();
-    que.push(que.front());
-    que.pop();
-  }
-
-  cout << que.front();
+  return 0;
 }
