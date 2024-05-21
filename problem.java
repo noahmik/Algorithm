527d9c817103a1366eeac2c1f4c9b760d41dec90
noahmik <s46422574@gmail.com>
2024-05-22 00:04:08 +0900

    컬렉션 프레임워크(Stack) 활용

--- problem.java
+++ problem.java
@@ -1,53 +1,37 @@
-import java.util.*;
+package com.example.pjh;
 
-class pjh{
-    public static void main(String[] args) {
-        ArrayList list1 = new ArrayList(10);
-        list1.add(new Integer(5));
-        list1.add(new Integer(4));
-        list1.add(new Integer(2));
-        list1.add(new Integer(0));
-        list1.add(new Integer(1));
-        list1.add(new Integer(3));
+import java.util.Stack;
 
-        //작성
-        ArrayList list2 = new ArrayList(list1.subList(1, 4));
-        print(list1, list2);
+class Main{
+    static Stack back = new Stack();
+    static Stack forward = new Stack();
 
-        //작성
-        Collections.sort(list1);
-        //작성
-        Collections.sort(list2);
-        print(list1, list2);
-
-        System.out.println("list1.containsAll(list2):" + list1.containsAll(list2));
-
-        list2.add("B");
-        list2.add("C");
-        //작성
-        list2.add(3, "A");
-        print(list1, list2);
-
-        //작성
-        list2.set(3, "AA");
-        print(list1, list2);
-
-        // list1에서 list2와 겹치는 부분만 남기고 나머지는 삭제한다.
-        System.out.println("list1.retainAll(list2):" + list1.retainAll(list2));
-        print(list1, list2);
-
-//          list2에서 list1에 포함된 객체들을 삭제한다.
-        for(int i= list2.size()-1; i >= 0; i--) {
-            if(list1.contains(list2.get(i)))
-                list2.remove(i);
-            //작성;
-        }
-        print(list1, list2);
-    } // main의 끝
-
-    static void print(ArrayList list1, ArrayList list2) {
-        System.out.println("list1:"+list1);
-        System.out.println("list2:"+list2);
-        System.out.println();
+    static void add(String name){
+        back.push(name);
     }
-} // class
+
+    static void front(){
+        back.push(forward.pop());
+    }
+
+    static void back(){
+        forward.push(back.pop());
+    }
+
+    public static void main(String[] args) {
+        add("1. 네이트");
+        add("2. 야후");
+        add("3. 네이버");
+        add("4. 다음");
+
+        System.out.println("back:"+back);
+        System.out.println("forward:"+forward);
+        System.out.println("현재 화면: "+back.peek()+" 입니다.");
+        System.out.println();
+
+        System.out.println("= '뒤로' 버튼을 누른 후 =");
+        System.out.println("back:"+back);
+        System.out.println("forward:"+back.peek());
+        System.out.println("현재 화면:"+back.peek()+" 입니다.");
+    }
+}
