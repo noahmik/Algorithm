cbfd6c14d0bd43b446401342be972f5cc6a7df9f
noahmik <s46422574@gmail.com>
2024-05-25 00:11:54 +0900

    컬렉션 프레임워크(Treeset) 활용

--- problem.java
+++ problem.java
@@ -1,37 +1,16 @@
-package com.example.pjh;
+package bsm.bto;
 
-import java.util.Stack;
+import java.util.Set;
+import java.util.TreeSet;
 
-class Main{
-    static Stack back = new Stack();
-    static Stack forward = new Stack();
-
-    static void add(String name){
-        back.push(name);
-    }
-
-    static void front(){
-        back.push(forward.pop());
-    }
-
-    static void back(){
-        forward.push(back.pop());
-    }
-
+public class t4 {
     public static void main(String[] args) {
-        add("1. 네이트");
-        add("2. 야후");
-        add("3. 네이버");
-        add("4. 다음");
-
-        System.out.println("back:"+back);
-        System.out.println("forward:"+forward);
-        System.out.println("현재 화면: "+back.peek()+" 입니다.");
-        System.out.println();
-
-        System.out.println("= '뒤로' 버튼을 누른 후 =");
-        System.out.println("back:"+back);
-        System.out.println("forward:"+back.peek());
-        System.out.println("현재 화면:"+back.peek()+" 입니다.");
+        TreeSet set = new TreeSet();
+        for (int i = 0; set.size() < 10 ; i++) {
+            int num = (int)(Math.random()*100) + 1;
+            set.add(num);
+        }
+        System.out.println(set.headSet(51));
+        System.out.println(set.tailSet(50));
     }
 }
